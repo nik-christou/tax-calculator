29aba318eb4b910abdbed419846b5abc1638b17d
Nikolas <nik.chr86@gmail.com>
2020-02-12 23:58:31 +0200

    Added cyprus data into a json file and added a simple dropdown that uses it for display

--- src/tax-calculator.js
+++ src/tax-calculator.js
@@ -1,8 +1,11 @@
-import { LitElement, html } from "lit-element";
+// @ts-check
 
+import { LitElement, html } from "lit-element";
 import { BaseElementMixin } from "./base/base-element-mixin.js";
 import { TaxCalculatorCss } from "./tax-calculator.css.js";
 import { SWRegistration } from "./sw-registration.js";
+
+import "./country-select.js";
 
 export class TaxCalculator extends BaseElementMixin(LitElement) {
     static get styles() {
@@ -13,6 +16,7 @@ export class TaxCalculator extends BaseElementMixin(LitElement) {
         return html`
             <div class="container">
                 <h2>Tax calculator</h2>
+                <country-select></country-select>
             </div>
         `;
     }
@@ -20,6 +24,29 @@ export class TaxCalculator extends BaseElementMixin(LitElement) {
     firstUpdated() {
 
         new SWRegistration().register();
+        this._loadCyprusJson();
+    }
+
+    _loadCyprusJson() {
+
+        const countrySelect = this.shadowRoot.querySelector('country-select');
+
+        this._loadJson('../data/cyprus.json')
+            .then(data => countrySelect.data = data)
+            .catch(reason => console.log(reason.message))
+    }
+
+    /**
+     * Load a data from a json file
+     *
+     * @param {String} jsonPath
+     */
+    async _loadJson(jsonPath) {
+
+        const response = await fetch(jsonPath);
+        const data = await response.json();
+
+        return data;
     }
 }
 
