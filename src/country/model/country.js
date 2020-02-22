79683daa9e0ecee3ea95ed7e34770ea8874ed4f5
Nikolas <nik.chr86@gmail.com>
2020-02-22 20:33:50 +0200

    Refactored the cyprus.json file to add currency and locale attributes, refactored country-loader to use new attirbutes as well the result-container to use the new attributes to create a number formatter

--- src/country/model/country.js
+++ src/country/model/country.js
@@ -7,19 +7,26 @@ export class Country {
      * @param {Array<TaxBracket>} taxBrackets
      * @param {Number} socialInsuranceContributionPercent
      * @param {Number} healthContributionPercent
+     * @param {String} locale
+     * @param {String} currency
      */
-    constructor(id, name, taxBrackets, socialInsuranceContributionPercent, healthContributionPercent) {
+    constructor(id, name, taxBrackets,
+        socialInsuranceContributionPercent, healthContributionPercent, locale, currency) {
         this.id = id;
         this.name = name;
         this.taxBrackets = taxBrackets;
         this.socialInsuranceContributionPercent = socialInsuranceContributionPercent;
         this.healthContributionPercent = healthContributionPercent;
+        this.locale = locale;
+        this.currency = currency;
 
         Object.freeze(this.id);
         Object.freeze(this.name);
         Object.freeze(this.taxBrackets);
         Object.freeze(this.socialInsuranceContributionPercent);
         Object.freeze(this.healthContributionPercent);
+        Object.freeze(this.locale);
+        Object.freeze(this.currency);
         Object.freeze(this);
     }
 }
