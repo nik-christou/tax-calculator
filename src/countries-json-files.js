ac0c0f695835158978a27989329fbd2407218561
Nikolas <nik.chr86@gmail.com>
2020-02-23 13:12:05 +0200

    Refactored country load mechanism to return a promise of array of countries, refactored country-select to use an array of country objects, removed not needed currency.js library since Intl.NumberFormat formatter works fine, refactored existing test and added new test case

---
+++ src/countries-json-files.js
@@ -0,0 +1,8 @@
+
+/**
+ *  This file just lists the json files locations
+ */
+
+export const countriesJson = [
+    "../web_assets/data/cyprus.json"
+];
--- test/tax-calculator.test.js
+++ test/tax-calculator.test.js
@@ -7,7 +7,7 @@ import { TaxResult } from '../src/results/model/tax-result.js';
 
 describe('TaxCalculator tests', () => {
 
-    it('annualIncomeTax_thirteenIncluded_correctResultsCalculated', () => {
+    it('cyprusAnnualIncomeTax_thirteenIncluded_correctResultsCalculated', () => {
 
         // given
         const taxBrackets = [
@@ -18,17 +18,44 @@ describe('TaxCalculator tests', () => {
             new TaxBracket(60000, -1, 35)
         ];
 
-        const selectedCountry = new Country(0, "cyprus", taxBrackets, 8.3, 1.70);
+        const country = new Country(0, "cyprus", taxBrackets, 8.3, 1.70);
         const salaryDetails = new SalaryDetails(20000.00, SalaryTypes.ANNUAL, true);
 
         // when
-        const taxResults = TaxCalculator.calculateTaxFromAnnualIncome(selectedCountry, salaryDetails);
+        const taxResults = TaxCalculator.calculateTaxFromAnnualIncome(country, salaryDetails);
 
         // then
-        const expectedMonthlyTaxResult = new TaxResult(1538.46, 7.68, 127.69, 26.15, 1376.94);
+        const expectedMonthlyTaxResult = new TaxResult(
+            1538.4615384615386, 7.676923076923076, 127.6923076923077, 26.153846153846153, 1376.9384615384615);
+
         const expectedAnnualTaxResults = new TaxResult(20000.00, 99.80, 1660, 340, 17900.20);
 
-        chai.assert.deepEqual(taxResults.monthlyTaxResult, expectedMonthlyTaxResult2, 'monthly tax results do not match');
+        chai.assert.deepEqual(taxResults.monthlyTaxResult, expectedMonthlyTaxResult, 'monthly tax results do not match');
+        chai.assert.deepEqual(taxResults.annualTaxResult, expectedAnnualTaxResults, 'annual tax results do not match');
+    });
+
+    it('cyprusMonthlyIncomeTax_thirteenIncluded_correctResultsCalculated', () => {
+
+        // given
+        const taxBrackets = [
+            new TaxBracket(0, 19500, 0),
+            new TaxBracket(19501, 28000, 20),
+            new TaxBracket(28001, 36300, 25),
+            new TaxBracket(36301, 60000, 30),
+            new TaxBracket(60000, -1, 35)
+        ];
+
+        const country = new Country(0, "cyprus", taxBrackets, 8.3, 1.70);
+        const salaryDetails = new SalaryDetails(20000.00, SalaryTypes.MONTHLY, true);
+
+        // when
+        const taxResults = TaxCalculator.calculateTaxFromMonthlyIncome(country, salaryDetails);
+
+        // then
+        const expectedMonthlyTaxResult = new TaxResult(20000, 99.8, 1660, 340, 17900.2);
+        const expectedAnnualTaxResults = new TaxResult(260000, 1297.3999999999999, 21580, 4420, 232702.6);
+
+        chai.assert.deepEqual(taxResults.monthlyTaxResult, expectedMonthlyTaxResult, 'monthly tax results do not match');
         chai.assert.deepEqual(taxResults.annualTaxResult, expectedAnnualTaxResults, 'annual tax results do not match');
     });
 });
